8ec2c3240a66899afdc8488d2f0a72e81e6d6e98
Romel Pascua <romelpascua14@gmail.com>
2021-05-18 13:21:13 -0700

    include a href links

---
+++ app.js
@@ -0,0 +1,5 @@
+/* dom variables */
+const linkedin_img = document.getElementById("l");
+const github_img = document.getElementById("g");
+const email_img = document.getElementById("e");
+const resume_img = document.getElementById("r");
