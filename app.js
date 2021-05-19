8edd6a41a4c61bbaf96873150a401b03c9b4e060
Romel Pascua <romelpascua14@gmail.com>
2021-05-18 21:20:30 -0700

    add images folder, insert circle, add fadeIn

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
 /* dom variables */
-const linkedin_img = document.getElementById("l");
-const github_img = document.getElementById("g");
-const email_img = document.getElementById("e");
-const resume_img = document.getElementById("r");
